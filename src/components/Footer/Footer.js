5f665438b464c41cd18f65520f8b0f1bc14a86b2
alfarid <alfaridislam@gmail.com>
2023-07-14 20:42:04 +0530

    Commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Footer.css'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className='Footer123'>
+        Hey...I am footer
+      </div>
+    )
+  }
+}
+
+export default Footer
+
